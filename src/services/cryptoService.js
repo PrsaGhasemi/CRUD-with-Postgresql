6822f49600344ffa3668bf2409a77069e422c7d9
Parsa Ghasemi <parsa.ghasemi2012@gmail.com>
2023-08-26 19:13:14 +0330

    Create cryptoService.js
    here you can see a summery about every API, i used this structure to make it simpler to understand and to change things within the code with the least consequences to the functionality

---
+++ src/services/cryptoService.js
@@ -0,0 +1,20 @@
+const cryptoRepository = require( '../repositories/cryptoRepository');
+
+const createCrypto = (name, price, symbol)=> {
+    return cryptoRepository.createCrypto(name, price, symbol);
+}
+
+const getCryptoById = (id)=> {
+    return cryptoRepository.getCryptoById(id);
+}
+
+const updateCrypto = (id, name, price, symbol)=> {
+    return cryptoRepository.updateCrypto(id, name, price, symbol)
+}
+
+const deleteCrypto = (id)=> {
+    return cryptoRepository.deleteCrypto(id)
+}
+
+
+module .exports = {createCrypto,getCryptoById,updateCrypto,deleteCrypto}
