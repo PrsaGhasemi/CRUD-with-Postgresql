248e70dc8654ba0614ab34d9d1e7389fdd4a5680
Parsa Ghasemi <parsa.ghasemi2012@gmail.com>
2023-08-26 19:15:39 +0330

    repository of my APIs

---
+++ src/repositories/cryptoRepository.js
@@ -0,0 +1,36 @@
+ const prisma =require('../db/prismaConnector');
+const createCrypto = (name, price, symbol)=> {
+    return prisma.crypto.create({
+        data: {
+            name,
+            cryptoDetail: {
+                create: {
+                    price,
+                    symbol,
+                },
+            },
+        },
+        include: { cryptoDetail: true },
+    });
+}
+
+const getCryptoById = (id) =>{
+    return prisma.crypto.findUnique({ where: { id }, include: { cryptoDetail: true } });
+}
+
+const updateCrypto = (id, name, price, symbol)=> {
+    return prisma.crypto.update({
+        where: { id },
+        data: {
+            name,
+            cryptoDetail: { update: { price, symbol } },
+        },
+        include: { cryptoDetail: true },
+    });
+}
+
+const deleteCrypto = (id)=> {
+    return prisma.crypto.delete({ where: { id } });
+}
+
+module.exports = {deleteCrypto,updateCrypto,getCryptoById,createCrypto};
